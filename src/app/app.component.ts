c72207436987fe813a29de8b171867cf5aa1d23d
Guest User <Guest@Epicodus-7C.local>
2018-05-01 16:38:32 -0700

    fix show/hide functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { Recipe } from './models/recipe.model';
+import { RECIPES } from './seed-recipes'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Recipe List';
+  recipes: Recipe[] = [
+    new Recipe(RECIPES[0].name, RECIPES[0].description, RECIPES[0].ingredients, RECIPES[0].instructions),
+    new Recipe(RECIPES[1].name, RECIPES[1].description, RECIPES[1].ingredients, RECIPES[1].instructions),
+    new Recipe(RECIPES[2].name, RECIPES[2].description, RECIPES[2].ingredients, RECIPES[2].instructions)
+  ];
+
+  selectRecipe = null;
+  hideRecipe() {
+    this.selectRecipe = null;
+  }
+  currentRecipe = this.recipes;
+  showRecipe() {
+    this.selectRecipe = this.currentRecipe;
+  }
 }
---
+++ src/app/seed-recipes.ts
@@ -0,0 +1,32 @@
+export const RECIPES = [
+  {
+    name: 'chocolate chip cookies',
+    description: 'ooey gooey',
+    ingredients: [
+      '3/4 cup granulated sugar. 3/4 cup packed brown sugar. 1 cup butter or margarine. softened. 1 teaspoon vanilla. 1 egg. 2 1/4 cups all-purpose flour 1 teaspoon baking soda. 1/2 teaspoon salt. 1 cup coarsely chopped nuts. 1 package (12 ounces) semisweet chocolate chips (2 cups)'
+    ],
+    instructions: [
+      'Heat oven to 375ºF. Mix sugars, butter, vanilla and egg in large bowl. Stir in flour, baking soda and salt (dough will be stiff). Stir in nuts and chocolate chips. Drop dough by rounded tablespoonfuls about 2 inches apart onto ungreased cookie sheet. Bake 8 to 10 minutes or until light brown (centers will be soft). Cool slightly; remove from cookie sheet. Cool on wire rack'
+    ]
+  },
+  {
+    name: 'mashed potatos',
+    description: 'creamy AF',
+    ingredients: [
+      '5lbs Russet Potatoes (scrubbed and peeled). 1/2 stick salted butter. 4oz sour scream. 4oz cream cheese. 6 cloves garlic (smashed). salt and pepper to taste'
+    ],
+    instructions: [
+      'Bring large stock pot full of water to a boil. While water is boiling, peel the potatoes and smash the garlic. Cut potatoes into large chunks. Once water is boiling, put potatoes and garlic in and let boil uncovered for 1 hour or until potatoes can be easily pierced with a fork. Drain water and return potatoes to pot. Add in rest of ingredients and mash until fully incorporated'
+    ]
+  },
+  {
+    name: 'lemon chicken pasta',
+    description: 'eat it',
+    ingredients: [
+      '4 boneless skinless chicken breast halves (6 ounces each). 1 teaspoon salt, divided. 1/4 teaspoon plus 1/8 teaspoon pepper, divided. 1/2 cup all-purpose flour. 8 ounces uncooked capellini or angel hair pasta. 3 tablespoons olive oil, divided. 12 cloves peeled and thinly sliced garlic. 1 cup white wine or chicken broth. 2 tablespoons lemon juice. 1/2 cup grated Parmigiano-Reggiano cheese. 1/3 cup plus 3 tablespoons minced fresh parsley, divided. Lemon wedges'
+    ],
+    instructions: [
+      'Pound chicken breasts with a meat mallet to 1/4-in. thickness. Sprinkle with 1/2 teaspoon salt and 1/4 teaspoon pepper. Place flour in a shallow bowl. Dip chicken in flour to coat both sides; shake off excess. Cook pasta according to package directions for al dente. Meanwhile, in a large skillet, heat 2 tablespoons oil over medium heat. Add chicken; cook 2-3 minutes on each side or until no longer pink. Remove and keep warm. In same pan, heat remaining oil over medium heat; add garlic. Cook and stir 30-60 seconds or until garlic is lightly browned. Add wine to pan; increase heat to medium-high. Cook, stirring to loosen browned bits from pan, until liquid is reduced by half. Stir in lemon juice. Drain pasta, reserving 1/2 cup pasta water; place in a large bowl. Add cheese, 1/3 cup parsley, half of the garlic mixture, and remaining salt and pepper; toss to combine, adding enough reserved pasta water to moisten pasta. Serve with chicken. Drizzle with remaining garlic mixture; sprinkle with remaining parsley. If desired, serve with lemon wedges'
+    ]
+  }
+];
